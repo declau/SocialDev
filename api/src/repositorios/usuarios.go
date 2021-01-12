6424e84dd6eb97ab7d561da26d9c1c19a6ebe287
declau <dec82@hotmail.com>
2021-01-12 19:52:53 -0300

    Cria usuário pelo repositório

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -16,6 +16,26 @@ func NovoRepositorioDeUsuarios(db *sql.DB) *Usuarios {
 }
 
 //Criar insere um usuário no banco de dados
-func (u Usuarios) Criar(Usuario models.Usuario) (uint64, error) {
-	return 0, nil
+func (repositorio Usuarios) Criar(Usuario models.Usuario) (uint64, error) {
+	statement, erro := repositorio.db.Prepare(
+		"insert into usuarios (nome, nick, email, senha) values(?, ?, ?, ?)",
+	)
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	resultado, erro := statement.Exec(
+		Usuario.Nome, Usuario.Nick, Usuario.Email, Usuario.Senha,
+	)
+	if erro != nil {
+		return 0, erro
+	}
+
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIDInserido), nil
 }
